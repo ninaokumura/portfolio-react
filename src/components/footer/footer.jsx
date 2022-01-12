f591d6630de75679fa25411c3b62376db9805fe0
Nina Okumura <elaineokumura@gmail.com>
2022-01-12 17:02:24 +1300

    add react-icons

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { FaHeart } from 'react-icons/fa';
 import './footer.css';
 
 function Footer() {
-  return <footer className='footer'>Made with love by Nina</footer>;
+  return (
+    <footer className='footer'>
+      Made with <FaHeart /> by Nina
+    </footer>
+  );
 }
 
 export default Footer;
