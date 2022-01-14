a47f8e67e51f139bc2b551d16cb51d79aa198525
Nina Okumura <elaineokumura@gmail.com>
2022-01-14 14:04:26 +1300

    add about me intro

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -5,6 +5,14 @@ import Timeline from '../components/timeline/timeline';
 function AboutMe() {
   return (
     <div>
+      <div className='about-me'>
+        <h3>Hi, I am Nina!</h3>
+        <p style={{ fontSize: '1.5rem' }}>
+          I was born and raised in Brazil in a beautiful city called Angra dos
+          Reis. I moved to New Zealand in 2017....
+        </p>
+      </div>
+      <h3>My learning Journey</h3>
       <Timeline />
     </div>
   );
