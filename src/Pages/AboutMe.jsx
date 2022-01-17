15ebf0621b9abd771f6ba171a72b706ad71f56da
Nina Okumura <elaineokumura@gmail.com>
2022-01-17 17:53:00 +1300

    add display grid to aboutMe section

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
 import Timeline from '../components/timeline/timeline';
+import Info from '../components/info/info';
 
 function AboutMe() {
   return (
     <div>
-      <div className='about-me'>
-        <h3>Hi, I am Nina!</h3>
-        <p style={{ fontSize: '1.5rem' }}>
-          I was born and raised in Brazil in a beautiful city called Angra dos
-          Reis. I moved to New Zealand in 2017....
-        </p>
-      </div>
-      <h3>My learning Journey</h3>
+      <Info />
+
       <Timeline />
     </div>
   );
---
+++ src/components/info/info.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import './info.css';
+
+function Info() {
+  return (
+    <div className='grid-container'>
+      <img className='info-img' src='/images/me.jpg' />
+      <div className='info-container'>
+        <h3 className='info-title'>Hi, I am Nina!</h3>
+        <p className='info-text'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim
+          voluptates quisquam unde praesentium sunt magnam alias, eaque nam odit
+          dolore natus, optio doloribus sint reprehenderit quis odio facilis
+          corporis neque. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Dolores velit modi, non optio tempora quidem. Ea eos unde quas
+          hic labore neque, error dolores velit distinctio. Excepturi facilis
+          tempore ratione.
+        </p>
+        <span className='info-icon'>
+          <a href='https://www.linkedin.com/in/nina-7b4377221/'>
+            <FaLinkedin />
+          </a>
+          <a href='https://github.com/ninaokumura'>
+            <FaGithub />
+          </a>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
