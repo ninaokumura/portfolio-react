523d36936a504e362272bb7b501c2816dd399e57
Nina Okumura <elaineokumura@gmail.com>
2022-01-31 21:33:32 +1300

    add contact info

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -5,31 +5,38 @@ import './form.css';
 function Form() {
   return (
     <div className='form-container'>
-      <form
-        className='form'
-        method='post'
-        action='mailto:elaineokumura@gmail.com'
-        method='post'
-        encType='text/plain'
-      >
-        <label>
-          <input type='text' placeholder='Name' />
-        </label>
-        <br />
-        <label>
-          <input type='text' placeholder='Email' />
-        </label>
-        <br />
-        <label>
-          <textarea placeholder='Message' rows='10' cols='30'></textarea>
-        </label>
-        <input
-          className='submit-btn'
-          type='submit'
-          name='message'
-          value='Submit'
-        />
-      </form>
+      <div className='details-container'>
+        <form
+          className='form'
+          method='post'
+          action='mailto:elaineokumura@gmail.com'
+          method='post'
+          encType='text/plain'
+        >
+          <label>
+            <input type='text' placeholder='Name' />
+          </label>
+          <br />
+          <label>
+            <input type='text' placeholder='Email' />
+          </label>
+          <br />
+          <label>
+            <textarea placeholder='Message' rows='10' cols='30'></textarea>
+          </label>
+          <input
+            className='submit-btn'
+            type='submit'
+            name='message'
+            value='Submit'
+          />
+        </form>
+        <div>
+          <h3 className='contact'>email:elaineokumura@gmail.com</h3>
+          <h3 className='contact'>phone: (+64)027 548 7193</h3>
+          <button className='btn'>Download resume</button>
+        </div>
+      </div>
     </div>
   );
 }
