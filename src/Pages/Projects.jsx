b343d169a282aeac4fa79dd24408233d47757f04
Nina Okumura <elaineokumura@gmail.com>
2022-02-07 18:52:56 +1300

    add project

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,82 +1,91 @@
 import React from 'react';
 import Card from '../components/card/card';
 
+// Icons
+import { FaNodeJs, FaReact, FaHtml5, FaBootstrap } from 'react-icons/fa';
+import {
+  SiJavascript,
+  SiMongodb,
+  SiCss3,
+  SiPug,
+  SiHandlebarsdotjs,
+} from 'react-icons/si';
+
 import './Projects.css';
 
+const icons = {
+  react: FaReact,
+  mongodb: SiMongodb,
+  node: FaNodeJs,
+  html: FaHtml5,
+  javascript: SiJavascript,
+  bootstrap: FaBootstrap,
+  css: SiCss3,
+  pug: SiPug,
+  handlebars: SiHandlebarsdotjs,
+};
+
 const projects = [
-  // {
-  //   img: './images/survey.jpg',
-  //   title: 'Survey',
-  //   body: 'dxkhfldflkjdklsjfkl',
-  // },
-  // {
-  //   img: './images/tribute-page.jpg',
-  //   title: 'Tribute Page',
-  //   body: 'djfhd jhfgdklsghlksdfjgl',
-  // },
-  // {
-  //   img: './images/technical-page.jpg',
-  //   title: 'Technical Page',
-  //   body: 'djfhd jhfgdklsghlksdfjgl',
-  // },
-  // {
-  //   img: './images/landing-page.jpg',
-  //   title: 'Landing Page',
-  //   body: 'djfhd jhfgdklsghlksdfjgl',
-  // },
   {
     img: './images/portfolio.jpg',
     title: 'Personal Portfolio',
-    body: 'Portfolio built as part of FreeCodeCamp course',
+    // body: 'Portfolio - FreeCodeCamp',
     codeUrl: 'https://codesandbox.io/s/personal-portfolio-764y8',
     demoUrl: 'https://764y8.csb.app/',
   },
   {
     img: './images/omnifood.jpg',
     title: 'Food App',
-    body: 'djfhd jhfgdklsghlksdfjgl',
+    // body: 'Omnifood is a food app delivery service.',
     codeUrl: 'https://github.com/ninaokumura/omnifood',
     demoUrl: 'https://omnifood-nina.netlify.app/',
   },
   {
     img: './images/guess-number.jpg',
     title: 'Guess the Number game',
-    body: 'Guess the number between 1 and 20',
+    // body: 'Guess the number between 1 and 20',
     codeUrl: 'https://github.com/ninaokumura/guess-the-number',
     demoUrl: 'https://ninaokumura.github.io/guess-the-number/',
   },
   {
     img: './images/forkify.jpg',
     title: 'Forkify',
-    body: 'Recipe app',
+    // body: 'Recipe app',
     codeUrl: 'https://github.com/ninaokumura/forkify-app',
     demoUrl: 'https://forkify-nina.netlify.app/',
   },
   {
     img: './images/personal-blog.jpg',
     title: 'Personal blog',
-    body: 'First project built as part of Bootcamp',
+    // body: 'First project built as part of Bootcamp',
     codeUrl: 'https://github.com/ninaokumura/ninaokumura.github.io',
     demoUrl: 'https://ninaokumura.github.io/',
   },
   {
     img: './images/blog.jpg',
     title: 'Brogui',
-    body: 'Just a blog.',
+    // body: 'Just a blog.',
+    codeUrl: 'https://github.com/ninaokumura/brogui',
+    demoUrl: 'https://brogui.herokuapp.com/',
+  },
+  {
+    img: './images/natours.jpg',
+    title: 'Natours',
+    // body: 'Travel ecommerce app',
     codeUrl: 'https://github.com/ninaokumura/brogui',
     demoUrl: 'https://brogui.herokuapp.com/',
   },
   {
     img: './images/calculator.jpg',
     title: 'Calculator',
-    body: 'Calculator buil with React',
+    // body: 'Calculator buil with React',
     codeUrl: 'https://github.com/ninaokumura/react-calculator',
     demoUrl: 'https://ninaokumura.github.io/react-calculator/',
   },
   {
     img: './images/crown.jpg',
     title: 'Crown clothing',
-    body: 'E-commerce app built using React',
+    // body: 'E-commerce app built using React',
     codeUrl: 'https://github.com/ninaokumura/react-crown-clothing',
     demoUrl: 'https://crown-clothing-42.herokuapp.com/',
   },
@@ -92,7 +101,7 @@ function Projects() {
           <Card>
             <Card.Img src={el.img} />
             <Card.Body>
-              <Card.Title>{el.title}</Card.Title>
+              <Card.Title className='card-title'>{el.title}</Card.Title>
               <Card.Text>{el.body}</Card.Text>
             </Card.Body>
             <a
