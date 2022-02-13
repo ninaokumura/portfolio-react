bdf29f45d2cee854b46c4d83ecb2dd17c69ae2cc
Nina Okumura <elaineokumura@gmail.com>
2022-02-13 13:17:51 +1300

    submit stateful form

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,20 +1,92 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
 import './form.css';
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
+
 function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const [submitError, setSubmitError] = useState('');
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = event => {
+    setEmail(event.target.value);
+  };
+
+  const handleMessageChange = event => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const formData = { name, email, message };
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...formData }),
+    })
+      .then(() => setSubmitted(true))
+      .catch(error => setSubmitError(error.message));
+  };
+
+  const isFormValid = useMemo(() => {
+    if (name === '') {
+      return false;
+    }
+    if (email === '') {
+      return false;
+    }
+    if (message === '') {
+      return false;
+    }
+    return true;
+  }, [name, email, message]);
+
   return (
     <div className='form-container'>
       <div className='details-container'>
-        <form className='form' method='post' name='contact' data-netlify>
+        {submitError && <div>Failed to submit form: {submitError}</div>}
+        {submitted && <div>Thanks for reaching out!</div>}
+        <form
+          className='form'
+          method='post'
+          name='contact'
+          data-netlify
+          onSubmit={handleSubmit}
+        >
           <input type='hidden' name='form-name' value='contact' />
 
           <label>
-            <input name='name' type='text' placeholder='Name' required />
+            <input
+              name='name'
+              type='text'
+              placeholder='Name'
+              value={name}
+              onChange={handleNameChange}
+              required
+            />
           </label>
           <br />
           <label>
-            <input name='email' type='email' placeholder='Email' required />
+            <input
+              name='email'
+              type='email'
+              placeholder='Email'
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
           </label>
           <br />
 
@@ -23,10 +95,17 @@ function Form() {
             placeholder='Message'
             rows='10'
             cols='30'
+            value={message}
+            onChange={handleMessageChange}
             required
           ></textarea>
 
-          <input className='submit-btn' type='submit' value='Submit' />
+          <input
+            className='submit-btn'
+            type='submit'
+            value='Submit'
+            disabled={!isFormValid}
+          />
         </form>
         <div className='contact-info'>
           <h2>Get in touch</h2>
